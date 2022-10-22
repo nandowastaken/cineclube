4e46d727a7d9520a841746089ab123ad2a79a130
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-10-22 20:26:36 -0300

    Add files via upload

---
+++ src/components/MemberCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="member">
+            <div class="box-container">
+                <img src="" alt="">
+            </div>
+            <div class="info-member">
+                <div class="member-header">
+                    <h1 class="member-name">{{ name }}</h1>
+                    <p class="member-function">{{ positionMember }}</p>
+                </div>
+                        
+                <p class="member-description">{{ description }}</p>
+            </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'YellowButton',
+        props: {
+            name: String,
+            positionMember: String, 
+            description: String,
+            image: String
+
+        }
+    });
+</script>
+
+<style>
+    .member {
+        display: flex;
+        flex-direction: row;
+        margin: 15px;
+    }
+
+    .info-member {
+        margin-left: 10px;
+    }
+
+    .member-name {
+        color: white;
+        font-family: "Inter", sans-serif;
+        font-size: 22px;
+        font-weight: 700;
+        margin-bottom: 0;
+        
+    }
+
+    .member-function {
+        color: #FFFFFF;
+        opacity: 0.8;
+    }
+    
+    .member-description {
+        font-family: "Inter", sans-serif;
+        font-size: 14px;
+    }
+
+    .box-container {
+        padding: 80px;
+        border-radius: 20px;
+        background-color: gray;
+    }
+</style>
--- src/screens/Team.vue
+++ src/screens/Team.vue
@@ -3,18 +3,23 @@
         <div class="column">
             <div class="team-member">
                 <h1 class="team-title" id="team">Equipe</h1>
-        
-                <div class="member">
-                    <div class="box-container"></div>
-                    <div class="info-member">
-                        <div class="member-header">
-                            <h1 class="member-name">Fernando Jorge</h1>
-                            <p class="member-function">presidente</p>
-                        </div>
-                        
-                        <p class="member-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
+                <MemberCard
+                    name="Fernando Jorge"
+                    position-member="presidente"
+                    description="Presidente do Cineclube desde Março de 2022. Adora Star Wars e por um milagre não botou o Darth Vader na capa quando criou esse site."
+                    image="Fernando.jpg"
+                />
+                <MemberCard
+                    name="Malu Alaquoke"
+                    position-member="mediadora"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                />
+                <MemberCard
+                    name="Lywya Eugênia"
+                    position-member="designer"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                />
+                
             </div>
         </div>
     </div>
@@ -22,9 +27,13 @@
 
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import MemberCard from '../components/MemberCard.vue';
 
     export default defineComponent({
         name: 'TeamCine',
+        components: {
+            MemberCard
+        }
         
     });
 </script>
@@ -42,41 +51,5 @@
         font-family: "Inter", sans-serif;
     }
 
-    .member {
-        display: flex;
-        flex-direction: row;
-        margin: 15px;
-    }
-
-    .info-member {
-        margin-left: 10px;
-    }
-
-    .member-name {
-        color: white;
-        font-family: "Inter", sans-serif;
-        font-size: 26px;
-        font-weight: 700;
-        margin-bottom: 0;
-        
-    }
-
-    .member-function {
-        color: #FFFFFF;
-        opacity: 0.8;
-    }
     
-    .member-description {
-        font-family: "Inter", sans-serif;
-        font-size: 14px;
-    }
-
-    .box-container {
-        width: 320px;
-        padding: 20px 20px;
-        border: 5px solid gray;
-        margin: 0;
-        border-radius: 20px;
-        background-color: gray;
-    }
 </style>
