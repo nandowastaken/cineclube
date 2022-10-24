0cff59108fa7fd6293645fde4d1f8633521d2a7c
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-10-24 13:43:34 -0300

    update footer

--- src/screens/Footer.vue
+++ src/screens/Footer.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="columns">
         <div class="column">
-            <!--
+            
             <div class="follow-us">
-                <p class="follow-us-text">Siga a gente nas redes sociais!</p>
+                <p class="follow-us-text">CINECLUBE</p>
+                <p class="club-description">O lugar perfeito para encontrar amigos com a mesma paixão e interesse que você tem pelo cinema.</p>
+
                 <div class="instagram">
                     <img src="../assets/svgs/instagram.svg" class="insta-logo">
                     <p>@cineclube_ifal</p>
                 </div>
+
+                <div class="email">
+                    <img src="../assets/imgs/email.png" class="email-icon">
+                    <p>cineclube.ifal@gmail.com</p>
+                </div>
                 
             </div>
-            -->
+            
 
             <div class="institute-info">
                 <img src="../assets/logos/logo-if.png" alt="Logo do Instituto Federal." class="logo-if">
@@ -19,7 +26,7 @@
             </div>
 
             <div class="copyright">
-                <p class="copyright-text">©2022 Cineclube</p>
+                <p class="copyright-text">© 2022 Cineclube</p>
                 <p class="copyright-text">Todos os direitos reservados.</p>
             </div>
             
