f95c182ddbd91088e33a6c78ac9453ccc2f5aaed
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-10-26 11:30:32 -0300

    change next-meeting text

--- src/screens/NextMeeting.vue
+++ src/screens/NextMeeting.vue
@@ -4,7 +4,8 @@
             <div class="content-next-meeting">
                 <h1 class="next-meeting-title" id="next-meeting">Próxima Reunião</h1>
                 <p class="next-meeting-description">
-                    Nossa próxima reunião é Sexta-feira, 18/11, sobre o filme Psicose (1960), do Alfred Hitchcock.
+                    Nossa próxima reunião é Sexta-feira, 25/11, sobre o filme Psicose (1960), do Alfred Hitchcock, nos horários da manhã
+                    de 11h até 12h, e de tarde, de 13h até 14h.
                 </p>
                 <a href="#movie">
                     <YellowButton message="Ver Filme"/>
