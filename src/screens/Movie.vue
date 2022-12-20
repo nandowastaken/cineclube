8a72c1d1fc864963b99dfa50a5b6284738b3c3a6
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-12-20 13:20:29 -0300

    Update Movie.vue

--- src/screens/Movie.vue
+++ src/screens/Movie.vue
@@ -3,17 +3,17 @@
         <div class="movieCardBox" >
             <div class="movieCard" id="movie">
                 <div class="imgMovieBox">
-                    <img src="https://www.silvanatoazza.com.br/cms/_files/general/835f0bae.jpg" class="movieCoverMobile">
-                    <img src="https://poltronanerd.com.br/wp-content/uploads/2018/01/Psicose-cena.jpg" class="movieCoverDesktop">
+                    <img src="https://occ-0-768-114.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTdQEk1cq4XPU1JajVf4wRyzuRIamC6Ra_RBVVZfD2-o6B40layNHPvcj1BAx7zHIP69hlgr17WncZxvF-FM3qYJUl8LaVDPzImu.jpg?r=b82" class="movieCoverMobile">
+                    <img src="https://occ-0-768-114.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTdQEk1cq4XPU1JajVf4wRyzuRIamC6Ra_RBVVZfD2-o6B40layNHPvcj1BAx7zHIP69hlgr17WncZxvF-FM3qYJUl8LaVDPzImu.jpg?r=b82" class="movieCoverDesktop">
                 </div>
 
                 <div class="infoMovie">
                     <div class="movieHeader">
-                        <p class="movieTitle">Psicose</p>
-                        <p class="movieDate">1960 - Terror/Thriller - 109min</p>
+                        <p class="movieTitle">A Origem dos Guardiões</p>
+                        <p class="movieDate">2012 - Animação/Fantasia - 97min</p>
                     </div>
                     
-                    <p class="movieSinopse">Uma mulher rouba uma fortuna de um homem milionário e entra em uma fuga, paranoica em ser pega, o desconforto é transmitido na trama, até ela eventualmente conhecer um sujeito dono de um hotel um tanto que suspeito.</p>
+                    <p class="movieSinopse">Jack Frost, um garoto que controla o inverno, se junta ao seleto time dos Guardiões Imortais para impedir Breu, o bicho-papão, de transformar todos os sonhos das crianças em pesadelo e usar seus poderes maquiavélicos para governar o mundo.</p>
                 </div>
 
                 <div class="movieBoxInstruction">
