27e72693aa854e43ced6f3c23c8bc6f76625838c
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-12-20 13:27:18 -0300

    Update NextMeeting.vue

--- src/screens/NextMeeting.vue
+++ src/screens/NextMeeting.vue
@@ -4,8 +4,7 @@
             <div class="content-next-meeting">
                 <h1 class="next-meeting-title" id="next-meeting">Próxima Reunião</h1>
                 <p class="next-meeting-description">
-                    Nossa próxima reunião é Sexta-feira, 25/11, sobre o filme Psicose (1960), do Alfred Hitchcock, nos horários da manhã
-                    de 11h até 12h, e de tarde, de 13h até 14h.
+                    Nossa próxima reunião nessa Sexta-feira, 23/12, onde iremos ter uma seção de cinema e assistiremos A Origem dos Guardiões (2012) no Ifal de 13:00 :)
                 </p>
 
                 <a href="#movie">
